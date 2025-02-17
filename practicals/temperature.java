6b651a98b9edb74a61897830fb985556401429ca
ANUSHKA-CHUTE <anuchute07@gmail.com>
2025-02-17 21:05:43 +0530

    temperature program

---
+++ practicals/temperature.java
@@ -0,0 +1,16 @@
+package practicals;
+public class temperature {
+    public static void main(String[] args)
+    {
+        // temperature in fahrenheit
+        double f = 50.0, c = 0.0;
+
+        // calculate celsius temperature
+        c = (f - 32) / 1.8;
+
+        // print the celsius temperature
+        System.out.println(
+            "value of temperature in celsius:" + c);
+    }
+}
+
